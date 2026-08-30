cfb803cacb6f7f0bf4d7882ef9739fa889ae754e
OliviaLivak <olivialivak12@gmail.com>
2026-08-30 17:47:47 -0300

    config supabase

--- lib/dependencies.dart
+++ lib/dependencies.dart
@@ -1,8 +1,15 @@
 import 'package:mobile/core/environment/environment.dart';
 import 'package:mobile/core/http/http_dependencies.dart';
+import 'package:mobile/core/serviceLocator/service_locator.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class DependenciesContainer {
   DependenciesContainer() {
     ApiDependencies(baseUrl: Environment.baseUrl);
+
+    serviceLocator.registerSingleton<SupabaseClient>(
+      Supabase.instance.client,
+      instanceName: 'SupabaseClient',
+    );
   }
 }
