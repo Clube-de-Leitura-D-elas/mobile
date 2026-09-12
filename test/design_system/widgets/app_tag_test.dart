05a3eec2fef681a408dab430e2fa39d7f8468439
PedroHCastilhos <pedro.castilhos.fraga@gmail.com>
2026-09-12 00:33:36 -0300

    refactor:removing unused import

--- test/design_system/widgets/app_tag_test.dart
+++ test/design_system/widgets/app_tag_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile/design_system/design_system.dart';
-import 'package:mobile/design_system/widgets/app_tag.dart';
 
 void main() {
   group('AppTag', () {
